8fa5618fef9297776dab62db064669dcfed4756f
Pedro Julian Palavecino <samuguayfenix@gmail.com>
2022-06-05 17:58:48 -0300

    Ejercicio complementario 1 - Level 1 (Se removieron saltos de linea innecesarios)

--- EjerciciosComplementariosLevel1/Ejercicio1.java
+++ EjerciciosComplementariosLevel1/Ejercicio1.java
@@ -1,7 +1,6 @@
 import java.util.Scanner;
 
 public class Ejercicio1 {
-
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
         System.out.println("Introduzca su nombre de usuario:");
